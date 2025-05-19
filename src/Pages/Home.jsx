fbc524a9879696591c620b940b1c32ddecbf7503
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2025-05-19 00:01:30 -0700

    yelp reviews

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,7 +9,21 @@ const Home = () => {
     <>
     <NavBar />
     <Hero />
-    <div className="main-content">Main Content</div>
+    <div className="main-content">HOURS/LOCATIONS</div>
+
+
+    {/* YELP REVIEWS */}
+    <div className='yelp-reviews'>
+      <span class="yelp-review" data-review-id="ykZNIDJ__hnOCQnHj_B4Iw" data-hostname="www.yelp.com">Read
+      </span>
+
+      <span class="yelp-review" data-review-id="l105iNc2NTwtO2RVMVhQIA" data-hostname="www.yelp.com">Read <a href="https://www.yelp.com/user_details?userid=IDaIRKylBmB_mcyVG1ZgIw" rel="nofollow noopener">Junoesque H.</a>'s <a href="https://www.yelp.com/biz/agis-caribbean-hollywood?hrid=l105iNc2NTwtO2RVMVhQIA" rel="nofollow noopener">review</a> of <a href="https://www.yelp.com/biz/LsXUOUUIf9PcnSPDfYMTXQ" rel="nofollow noopener">Agis Caribbean </a> on <a href="https://www.yelp.com" rel="nofollow noopener">Yelp</a><script src="https://www.yelp.com/embed/widgets.js" type="text/javascript" async></script></span>
+
+
+      <span class="yelp-review" data-review-id="Mz44Djqg0UrkOo1fNgcqCQ" data-hostname="www.yelp.com">Read <a href="https://www.yelp.com/user_details?userid=L98kQ3Nqps2hOxPLPtUVlA" rel="nofollow noopener">Sarina H.</a>'s <a href="https://www.yelp.com/biz/agis-caribbean-hollywood?hrid=Mz44Djqg0UrkOo1fNgcqCQ" rel="nofollow noopener">review</a> of <a href="https://www.yelp.com/biz/LsXUOUUIf9PcnSPDfYMTXQ" rel="nofollow noopener">Agis Caribbean </a> on <a href="https://www.yelp.com" rel="nofollow noopener">Yelp</a><script src="https://www.yelp.com/embed/widgets.js" type="text/javascript" async></script></span>
+    </div>
+
+
     <Footer />
     </>
   )
